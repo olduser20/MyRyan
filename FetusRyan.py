f366add625b1ddb943cf07319f1b74f1ecefbd48
Saber Nemati <m.saber.n@gmail.com>
2019-06-03 10:05:09 +0430

    Adding Sphinx and Google speech recognitions

--- FetusRyan.py
+++ FetusRyan.py
@@ -14,4 +14,22 @@ with sr.Microphone() as source:
     audio=r.listen(source)
 
 
+# Using Sphinx
+try:
+    print("Sphinx thinks you said "+r.recognize_sphinx(audio))
+except  sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+
+# Using Google speech recognition
+try:
+    print("Google Speech Recognition thinks you said "+r.recognize_google(audio))
+except  sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
 
