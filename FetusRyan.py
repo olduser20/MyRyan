cfcf41b315cc31e82e169ed13c6dc3a30f79cb64
Saber Nemati <saberiri@yahoo.com>
2019-06-05 16:36:22 +0430

    Getting rid of Sphinx for now.

--- FetusRyan.py
+++ FetusRyan.py
@@ -36,12 +36,12 @@ with sr.Microphone() as source:
 
 
 # Using Sphinx
-try:
-    print("Sphinx thinks you said "+r.recognize_sphinx(audio))
-except  sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+# try:
+#     print("Sphinx thinks you said "+r.recognize_sphinx(audio))
+# except  sr.UnknownValueError:
+#     print("Sphinx could not understand audio")
+# except sr.RequestError as e:
+#     print("Sphinx error; {0}".format(e))
 
 
 # Using Google speech recognition
