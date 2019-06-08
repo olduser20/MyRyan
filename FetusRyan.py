a27ebcb3648b4237de36f406698ffd0180abc7a4
Saber Nemati <m.saber.n@gmail.com>
2019-06-08 18:19:06 +0430

    Adding the written mode.

--- FetusRyan.py
+++ FetusRyan.py
@@ -33,34 +33,50 @@ class News:
 
 
 # Get audio input (sample)
-
-r=sr.Recognizer()
-
-with sr.Microphone() as source:
-    print("Say something!")
-    audio=r.listen(source)
-
-
-# Using Sphinx
-# try:
-#     print("Sphinx thinks you said "+r.recognize_sphinx(audio))
-# except  sr.UnknownValueError:
-#     print("Sphinx could not understand audio")
-# except sr.RequestError as e:
-#     print("Sphinx error; {0}".format(e))
-
-
-# Using Google speech recognition
 try:
-    command=r.recognize_google(audio)
-    print("Google Speech Recognition thinks you said "+command)
-except  sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    r=sr.Recognizer()
 
-if (command=="weather"):
-    print("Call the Weather class.")
-elif (command=="stock market"):
-    print("Call the StockMarket class.")
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio=r.listen(source)
+
+    # Using Sphinx
+    # try:
+    #     print("Sphinx thinks you said "+r.recognize_sphinx(audio))
+    # except  sr.UnknownValueError:
+    #     print("Sphinx could not understand audio")
+    # except sr.RequestError as e:
+    #     print("Sphinx error; {0}".format(e))
+
+
+    # Using Google speech recognition
+    try:
+        command=r.recognize_google(audio)
+        print("Google Speech Recognition thinks you said "+command)
+    except  sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    if (command=="weather"):
+        print("Call the Weather class.")
+    elif (command=="stock market"):
+        print("Call the StockMarket class.")
+
+
+except OSError:
+    print("No microphone device is detected!")
+    # print("Do you want to use the written mode?")
+    strMode=input("Do you want to load the written mode [y/n]? ")
+    if strMode=="y" or strMode=="Y":
+        print("Written mode activated.")
+    elif strMode=="n" or strMode=="N":
+        print("Closing the program...")
+    else:
+        print("Wrong input!")
+
+
+
+
+
 
